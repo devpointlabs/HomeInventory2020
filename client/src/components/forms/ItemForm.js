f85adab04a0fe5b1e728373146ee8b40e908c08e
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-04-02 16:36:24 -0600

    built out items/locations forms

---
+++ client/src/components/forms/ItemForm.js
@@ -0,0 +1,173 @@
+import React from "react";
+import axios from "axios";
+import { Form, Input, DatePicker, InputNumber, Button } from "antd";
+import moment from 'moment'
+
+class ItemForm extends React.Component {
+  state = { name: "", make: "", model: "", serial_num: "", category: "", collection: "", condition: "", heir: "", purchase_date: null, quantity: "", value: "", tags: "", };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("/api/items", { ...this.state, })
+      .then( res => {
+        this.props.add(res.data);
+      })
+  }
+
+  handleNumberInputChange = (value) => {
+    this.setState({quantity: value})
+  };
+
+  handleFloatInputChange = (value) => {
+    this.setState({value: value})
+  };
+
+  handleChange = (e) => {
+    const { name, value, } = e.target;
+    this.setState({ [name]: value, });
+  };
+
+  handleDate = (date) => {
+    this.setState({purchase_date: date})
+  }
+
+  render() {
+    const { name, make, model, serial_num, category, collection, condition, heir, purchase_date, quantity, value, tags } = this.state;
+    return (
+      <> 
+        <Form onFinish={this.handleSubmit}>
+          <Form.Item >
+            <Input
+            label="Name"
+            required     
+            autoFocus
+            name='name'
+            value={name}
+            placeholder='Name'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <Input
+            label="Make"
+            required     
+            name='make'
+            value={make}
+            placeholder='Make'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <Input
+            label="Model"
+            required     
+            name='model'
+            value={model}
+            placeholder='Model'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <Input
+            label="Serial_num"
+            required     
+            name='serial_num'
+            value={serial_num}
+            placeholder='Serial_num'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              label="Category"
+              required
+              name='category'
+              value={category}
+              placeholder='Category'
+              onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+            label="Collection"
+            required
+            name='collection'
+            value={collection}
+            placeholder='Collection'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+            label="Condition"
+            required
+            name='condition'
+            value={condition}
+            placeholder='Condition'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+            label="Heir"
+            required
+            name='heir'
+            value={heir}
+            placeholder='Heir'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <DatePicker 
+            label="Purchase Date"
+            required
+            name='purchase_date'
+            value={purchase_date}
+            onChange={this.handleDate}
+            />
+          </Form.Item>
+          <Form.Item >
+            <InputNumber 
+            type="integer"
+            label="Quantity"
+            required
+            name='quantity'
+            value={quantity}
+            placeholder='Quantity'
+            onChange={this.handleNumberInputChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <InputNumber
+              label="Value"
+              required
+              name='value'
+              value={value}
+              defaultValue={0}
+              placeholder='value'
+              formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              parser={value => value.replace(/\$\s?|(,*)/g, '')}
+              onChange={this.handleFloatInputChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+            label="Tags"
+            required
+            name='tags'
+            value={tags}
+            placeholder='Tags'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </>
+    );
+  };
+}
+export default ItemForm;
---
+++ client/src/components/forms/LocationForm.js
@@ -0,0 +1,72 @@
+import React from "react";
+import axios from "axios";
+import { Form, Input, InputNumber, Button } from "antd";
+
+class LocationForm extends React.Component {
+  state = { name: "", square_footage: "", description: ""};
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("/api/locations", { ...this.state, })
+      .then( res => {
+        this.props.add(res.data);
+      })
+  }
+
+  handleChange = (e) => {
+    const { name, value, } = e.target;
+    this.setState({ [name]: value, });
+  };
+
+  handleNumberInputChange = (value) => {
+    this.setState({square_footage: value})
+  };
+
+
+  render() {
+    const { name, square_footage, description} = this.state;
+    return (
+      <> 
+        <Form onFinish={this.handleSubmit}>
+          <Form.Item >
+            <Input
+            label="Name"
+            required     
+            autoFocus
+            name='name'
+            value={name}
+            placeholder='Name'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <InputNumber 
+            label="Square Footage"
+            required
+            name='square_footage'
+            value={square_footage}
+            placeholder='Square Footage'
+            onChange={this.handleNumberInputChange}
+            />
+          </Form.Item>
+          <Form.Item >
+            <Input
+            label="Description"
+            required     
+            name='description'
+            value={description}
+            placeholder='Description'
+            onChange={this.handleChange}
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </>
+    );
+  };
+}
+export default LocationForm;
