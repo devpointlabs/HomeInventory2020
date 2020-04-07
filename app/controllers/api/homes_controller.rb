d21520c3b513debe3af132a84b97768ae3689e01
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-04-07 06:21:02 -0600

    built out Ass/Maint/Home controllers

--- app/controllers/api/homes_controller.rb
+++ app/controllers/api/homes_controller.rb
@@ -1,16 +1,37 @@
 class Api::HomesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_home, only: [:show, :edit, :destroy]
+  
   def index
-  end
-
-  def show
-  end
-
-  def new
-  end
-
-  def update
+    render json: current_user.homes.all
   end
 
   def create
+    home = current_user.homes.new(home_params)
+    if home.save
+      render json: home
+    else
+      render json: home.errors, status: 422
+    end
   end
+
+  def update
+    @home.update(home_params)
+    render json: @home
+  end
+
+  def destroy
+    @home.destroy
+    render json: { message: 'Home deleted.' }
+  end
+
+private
+  def home_params
+    params.permit(:address, :zip_code, :square_footage, :lot_size, :purchase_date, :purchase_price, :image)
+  end
+
+  def set_home
+    @home = current_user.homes.find(params[:id])
+  end
+  
 end
