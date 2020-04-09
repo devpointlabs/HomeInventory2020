f55de510d2ce588994305e2a065dc54c92c92a1b
edd23 <yahired3@gmail.com>
2020-04-09 17:05:50 -0600

    fileform

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import axios from 'axios'
 import ItemInfo from './ItemInfo'
 import ItemPhoto from './ItemPhotos';
+import FileUpload from './FileUpload';
 import Receipts from './Receipts';
 import { Button } from 'antd'
 import { PlusOutlined, DeleteOutlined, EditOutlined } from '@ant-design/icons'
@@ -105,7 +106,7 @@ class Items extends React.Component {
         )
       case 'files':
         return (
-          <p>FILES</p>
+          <FileUpload itemId={this.state.itemId} locationId={this.state.locationId} />
         )
       case 'newLocation':
         return (
