8dc7234c2b903b2fe4d9798a20a407768aa2ab96
Charles Buxton <chazbux@gmail.com>
2020-04-14 14:44:38 -0600

    delete item reworked

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
-    resources :locations
+    resources :locations do
+      resources :items
+    end
     resources :items do
       resources :photos
       resources :documents
